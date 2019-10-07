3fa569cf3d19513bf3b30595864fd1a611b39dcf
christian williams <christian.williamsj@gmail.com>
2019-10-07 23:12:48 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artists
+    self.songs.collect {|song| song.artist}.uniq
+  end
+
+end
