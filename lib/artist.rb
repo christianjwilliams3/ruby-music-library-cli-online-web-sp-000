e54124f6fc39756caba7bf8fb4eaafdaf5db5232
christian williams <christian.williamsj@gmail.com>
2019-10-07 23:33:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,7 @@
-require 'pry'
-
 class Artist
-  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
-  extend Concerns::Findable
+  
+  attr_accessor :name, :songs
+  
   @@all = []
 
   def initialize(name)
@@ -14,48 +13,28 @@ class Artist
     @@all
   end
 
-  def self.destroy_all
+  def add_song(song)
+    @songs << song unless songs.include?(song)
+    if song.artist == nil
+      song.artist = self
+    end
+  end
+  
+   def self.destroy_all
     @@all.clear
   end
-
+  
   def save
     @@all << self
   end
-
-  def self.create(artist)
+  
+    def self.create(artist)
     artist = self.new(artist)
     artist.save
     artist
   end
 
-  def songs
-    @songs
-  end
-
-  def add_song(song)
-    if song.artist == nil
-      song.artist = self
-    else
-      nil
-    end
-    if @songs.include?(song)
-      nil
-    else
-      @songs << song
-    end
-    song
-  end
-
   def genres
-    @new_array = []
-    @songs.each do |song|
-      if @new_array.include?(song.genre)
-        nil
-      else
-        @new_array << song.genre
-      end
-    end
-    @new_array
+    self.songs.collect{|song| song.genre}.uniq
   end
-
 end
