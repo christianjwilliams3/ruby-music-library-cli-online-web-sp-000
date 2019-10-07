641ba05dc038677c4bd7087e4b977cf3dbdae101
christian williams <christian.williamsj@gmail.com>
2019-10-07 23:17:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -83,13 +83,8 @@ class Song
     song_name = name.split(" - ")[1]
     artist_name = name.split(" - ")[0]
     genre_name = name.split(" - ")[2].chomp(".mp3")
-    #create song and assign artist and genre attribute, & create connections
-    #prevent the creation of duplicate objects: songs, artists, genres
-    #song = self.create(song_name)
     song = self.find_or_create_by_name(song_name)
-    #song.artist = Artist.create(artist_name)
     song.artist = Artist.find_or_create_by_name(artist_name)
-    #song.genre = Genre.create(genre_name)
     song.genre = Genre.find_or_create_by_name(genre_name)
     song
   end
